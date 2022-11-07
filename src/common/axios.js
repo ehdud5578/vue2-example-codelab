2b4939cf1ca0657b0bff4a1b6a0618b91f229f70
yb-k <kyb2206@knou.ac.kr>
2022-11-07 16:59:46 +0900

    feat: axios jwt 리프레시 토큰 체크 / 재요청 로직 작성

--- src/common/axios.js
+++ src/common/axios.js
@@ -1,9 +1,28 @@
-import axios from 'axios';
+import { authService } from '@/services/api';
+import store from '@/store';
+import axios, { AxiosError } from 'axios';
 import { API_URL } from './config';
+import { GETTERS, STATUS_CODE, MUTATIONS } from './constants';
+
 const instance = axios.create({
   baseURL: API_URL,
   timeout: 30000,
 });
+
+const getAccessToken = async () => {
+  try {
+    const { refresh: refreshToken } = store.getters[GETTERS.AUTH.TOKENS];
+    const tokens = await authService.refreshToken(refreshToken);
+    store.commit(MUTATIONS.AUTH.TOKEN, {
+      access: tokens.access.token,
+      refresh: tokens.refresh.token,
+    });
+    return tokens;
+  } catch (error) {
+    store.commit(MUTATIONS.AUTH.TOKEN, { access: '', refresh: '' });
+  }
+  return '';
+};
 
 /**
  * 요청에 대한 interceptor
@@ -11,6 +30,10 @@ const instance = axios.create({
 instance.interceptors.request.use(
   function (config) {
     // 요청이 전달되기 전에 작업 수행
+    const { access } = store.getters[GETTERS.AUTH.TOKENS];
+    if (access) {
+      config.headers.Authorization = `Bearer ${access}`;
+    }
     return config;
   },
   function (error) {
@@ -26,12 +49,34 @@ instance.interceptors.response.use(
   function (response) {
     // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
     // 응답 데이터가 있는 작업 수행
-    return response;
+
+    return response.data;
   },
-  function (error) {
+  async function (error) {
     // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
     // 응답 오류가 있는 작업 수행
-    return Promise.reject(error);
+    if (!(error instanceof AxiosError)) {
+      // 런타임 에러 발생시 처리
+      return Promise.reject(error);
+    }
+    const {
+      config,
+      response: { status },
+    } = error;
+    if (config.url.includes('/refresh-tokens') || status !== 401 || config.sent) {
+      return Promise.reject(error);
+    }
+
+    if (status === STATUS_CODE.UNAUTHORIZED) {
+      // 액세스 토큰 만료시 또는 세션 만료시..
+      const accessToken = await getAccessToken();
+      if (accessToken) {
+        config.sent = true;
+        config.headers.Authorization = `Bearer ${accessToken}`;
+        return instance(config);
+      }
+    }
+    return Promise.reject(config);
   }
 );
 
--- src/common/constants.js
+++ src/common/constants.js
@@ -3,3 +3,80 @@
  * 만약, ENUM키나 관리되는 상수값이 많아지는 경우
  * 폴더로 분리하여 모듈화 하여 관리할 필요가 있습니다.
  */
+
+/**
+ * Store Mutations Keys
+ */
+export const MUTATIONS = {
+  AUTH: {
+    TOKEN: 'auth/token',
+    SESSION: 'auth/session',
+  },
+  BOARD: {
+    APPEND: 'board/append',
+    SET: 'board/set',
+    INIT: 'board/init',
+    DETAIL: 'board/detail',
+  },
+};
+/**
+ * Store Actions Keys
+ */
+export const ACTIONS = {
+  AUTH: {
+    LOGIN: 'auth/login',
+    LOGOUT: 'auth/logout',
+    SESSION: 'auth/session',
+    REFRESH_ACCESS: 'auth/refresh',
+  },
+  BOARD: {
+    APPEND: 'board/append',
+    DETAIL: 'board/detail',
+  },
+};
+/**
+ * Store Getters Keys
+ */
+export const GETTERS = {
+  AUTH: {
+    TOKENS: 'auth/token',
+    IS_ANONYMOUS: 'auth/isAnonymous',
+    SESSION: 'auth/session',
+  },
+  BOARD: {
+    LIST: 'board/list',
+    DETAIL: 'board/detail',
+  },
+};
+
+/**
+ * Layout Enum
+ * @description /layout 폴더 내 vue 파일명
+ */
+export const ENUM_LAYOUT = {
+  NONE: 'none',
+  DEFAULT: 'default',
+  SIMPLE: 'simple',
+};
+
+/**
+ * App Route Names for Navigation
+ */
+export const VIEW_NAVI = {
+  INTRO: 'intro',
+  LOGIN: 'login',
+  HOME: 'home',
+  BOARD_DETAIL: 'board-id',
+};
+
+/**
+ * App Constans Storage Keys
+ */
+export const STORAGE_KEYS = {
+  ACCESS_TOKEN: 'accessToken',
+  REFRESH_TOKEN: 'refreshToken',
+};
+
+export const STATUS_CODE = {
+  UNAUTHORIZED: 401,
+};
