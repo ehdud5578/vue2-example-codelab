206d941d2fc9f958ac90ca83ef5a5689051ca49a
yb-k <kyb2206@knou.ac.kr>
2022-11-08 11:40:26 +0900

    refactor: file upload and preview work

--- src/modules/file/AppFileUploadBtn.vue
+++ src/modules/file/AppFileUploadBtn.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card class="d-flex align-center" filled outlined height="200" @click="onClick">
+  <v-card class="d-flex align-center overflow-hidden" filled outlined height="200" @click="onClick">
     <div v-if="value" class="flex-grow-1 text-center">
-      <v-img height="200" src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
+      <v-img height="200" max-width="100%" :src="fullImagePath"></v-img>
     </div>
     <div v-else class="flex-grow-1 text-center">
       <v-icon>mdi-camera</v-icon>
@@ -12,12 +12,18 @@
 
 <script>
 import { fileService } from '@/services/api';
+import { API_URL } from '@/common/config';
 export default {
   name: 'app-file-upload-btn',
   props: {
     value: {
       type: String,
       require: true,
+    },
+  },
+  computed: {
+    fullImagePath() {
+      return `${API_URL}${this.value}`;
     },
   },
   methods: {
@@ -27,8 +33,12 @@ export default {
     async onChange(e, values) {
       const file = e.target.files[0];
       if (file) {
-        const data = await fileService.upload(file);
+        const { path, fullpath } = await fileService.upload(file);
+        this.$emit('input', path);
+      } else {
+        this.$emit('input', '');
       }
+      e.target.value = '';
     },
   },
 };
