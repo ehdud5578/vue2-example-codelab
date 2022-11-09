3986978a23c824d69b85c9154a6fc1d2a44cfd3d
yb-k <kyb2206@knou.ac.kr>
2022-11-09 11:09:06 +0900

    chore: 권한 체크 미들웨어 수정 및 ㅈ어리

--- src/middlewares/default.js
+++ src/middlewares/default.js
@@ -1,15 +1,13 @@
-import store from '@/store';
-import { VIEW_NAVI } from '../common/constants';
-import { GETTERS } from '@/common/constants';
 import Vue from 'vue';
+import store from '@/store';
+import { GETTERS, VIEW_NAVI } from '@/common/constants';
 
 const { INTRO, HOME, LOGIN, USER_REGIST } = VIEW_NAVI;
 const ANONYMOUS_VIEW = [INTRO, HOME, LOGIN, USER_REGIST];
 export default (to, from, next) => {
   if (!ANONYMOUS_VIEW.includes(to.name) && store.getters[GETTERS.AUTH.IS_ANONYMOUS]) {
-    Vue.prototype.$alert('세션정보를 찾을 수 없습니다.<br/> 홈으로 이동합니다.').then(() => {
-      next({ name: LOGIN });
-    });
+    next(false);
+    Vue.prototype.$alert('로그인이 필요한 서비스입니다.');
     return;
   }
   next();
