e67d67b7cbb8dba8036b1cdc0c7d541f1e69634d
yb-k <kyb2206@knou.ac.kr>
2022-11-09 09:04:39 +0900

    fix: vue-router NavigationDuplicated catch

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,23 @@ import Router from 'vue-router';
 import routes from 'vue-auto-routing';
 import { createRouterLayout } from 'vue-router-layout';
 
+/**
+ * Vue-Router NavigationDuplicated error fixed
+ * https://github.com/vuejs/vue-router/issues/2881#issuecomment-520554378
+ */
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject);
+  return originalPush.call(this, location).catch((err) => {
+    if (Router.isNavigationFailure(err)) {
+      // resolve err
+      return err;
+    }
+    // rethrow error
+    return Promise.reject(err);
+  });
+};
+
 Vue.use(Router);
 
 const RouterLayout = createRouterLayout((layout) => {
